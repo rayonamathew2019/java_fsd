8a2d37c2b20418a229b18b19fa0cfd7224f49401
rayonamathew2019 <47378995+rayonamathew2019@users.noreply.github.com>
2023-03-14 09:31:16 +0530

    Update Array.java

--- src/collection/Array.java
+++ src/collection/Array.java
@@ -1,3 +1,6 @@
+Write a program to add elements to a list and get familiarize with various operations using the list in java 8?
+
+
 package collection;
 import java.util.*;
 
@@ -11,17 +14,17 @@ public class Array {
 	     list11.add("Banana");    
 	     list11.add("Grapes");    
 	        
-	   //  Iterator itra=list11.iterator();
-	     // System.out.println(list11);  
-	   //  while(itra.hasNext()){//check if iterator has the elements  
-	    	  // System.out.println(itra.next());//printing the element and move to next  
+	     Iterator itra=list11.iterator();
+	      System.out.println(list11);  
+	     while(itra.hasNext()){//check if iterator has the elements  
+	    	   System.out.println(itra.next());//printing the element and move to next  
 	    	   
-	    	  //}  
+	    	  }  
 	   //Traversing list through for-each loop  
-	    // for(String fruit:list11)    
-	      // System.out.println(fruit);    
+	     for(String fruit:list11)    
+	       System.out.println(fruit);    
 	     System.out.println("Returning element: "+list11.get(1));//it will return the 2nd element, because index starts from 0  
-	     //changing the element  
+// 	     changing the element  
 	     list11.set(1,"Dates");  
 	     //Traversing list  
 	     for(String fruit:list11)    
